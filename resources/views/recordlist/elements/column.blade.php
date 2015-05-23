da2a2f1f74d1ed88e5b571c35f9ae90b4d0235dd
fen <fen@desmart.com>
2015-05-23 15:16:16 +0200

    Moving from Blade to Twig.

--- resources/views/recordlist/elements/column.blade.php
+++
@@ -1,7 +0,0 @@
-<th>
-    <span>
-        {{ $column }}
-    </span>
-    {{--<a class="order-dir order-desc{% if order_by == column and order_dir == 'desc' %} active{% endif %}" href="{{ order[column].desc }}">[DESC]</a>--}}
-    {{--<a class="order-dir order-asc{% if order_by == column and order_dir == 'asc' %} active{% endif %}" href="{{ order[column].asc }}">[ASC]</a>--}}
-</th>
