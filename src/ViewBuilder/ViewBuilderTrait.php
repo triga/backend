60579ba9454318421362c2812ddac42aa43bcd88
fen <fen@desmart.com>
2015-07-07 22:19:20 +0200

    Minor adjustments and comments in the view builder classes.

--- src/ViewBuilder/ViewBuilderTrait.php
+++ src/ViewBuilder/ViewBuilderTrait.php
@@ -1,17 +1,41 @@
 <?php namespace TrigaBackend\ViewBuilder;
 
+/**
+ * Trait helping build a complete backend layout.
+ *
+ * @package TrigaBackend\ViewBuilder
+ */
 trait ViewBuilderTrait
 {
+
+    /**
+     * Default layout path.
+     *
+     * @var string
+     */
     protected $layoutPath = 'trigabackend::layout.layout';
 
+    /**
+     * Passes all variables to the view and returns the result.
+     *
+     * @param ViewOrganizer $viewOrganizer
+     * @return \Illuminate\View\View
+     */
     protected function buildView(ViewOrganizer $viewOrganizer)
     {
         return view($this->layoutPath, [
+            'header' => $viewOrganizer->getHeader(),
             'side_bar' => $viewOrganizer->getSideBar(),
+            'bread_crumbs' => $viewOrganizer->getBreadCrumbs(),
             'main_content' => $viewOrganizer->getMainContent(),
         ]);
     }
 
+    /**
+     * Returns a new instance of the view organizer.
+     *
+     * @return ViewOrganizer
+     */
     protected function getViewOrganizer()
     {
         return new ViewOrganizer;
--- src/ViewBuilder/ViewOrganizer.php
+++ src/ViewBuilder/ViewOrganizer.php
@@ -1,12 +1,43 @@
 <?php namespace TrigaBackend\ViewBuilder;
 
+/**
+ * Helper class making building the bavkend view easier.
+ *
+ * @package TrigaBackend\ViewBuilder
+ */
 class ViewOrganizer
 {
+
+    /**
+     * Sidebar view.
+     *
+     * @var string
+     */
     protected $sideBar;
+
+    /**
+     * Main content view.
+     *
+     * @var string
+     */
     protected $mainContent;
 
     /**
-     * @return mixed
+     * Page header text.
+     *
+     * @var string
+     */
+    protected $header;
+
+    /**
+     * Breadcrumbs view.
+     *
+     * @var string
+     */
+    protected $breadCrumbs;
+
+    /**
+     * @return string
      */
     public function getSideBar()
     {
@@ -14,15 +45,18 @@ class ViewOrganizer
     }
 
     /**
-     * @param mixed $sideBar
+     * @param string $sideBar
+     * @return $this
      */
     public function setSideBar($sideBar)
     {
         $this->sideBar = $sideBar;
+
+        return $this;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getMainContent()
     {
@@ -30,10 +64,51 @@ class ViewOrganizer
     }
 
     /**
-     * @param mixed $mainContent
+     * @param string $mainContent
+     * @return $this
      */
     public function setMainContent($mainContent)
     {
         $this->mainContent = $mainContent;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    /**
+     * @param string $header
+     * @return $this
+     */
+    public function setHeader($header)
+    {
+        $this->header = $header;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBreadCrumbs()
+    {
+        return $this->breadCrumbs;
+    }
+
+    /**
+     * @param string $breadCrumbs
+     * @return $this
+     */
+    public function setBreadCrumbs($breadCrumbs)
+    {
+        $this->breadCrumbs = $breadCrumbs;
+
+        return $this;
     }
 }
